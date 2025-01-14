70ffd47ad91d9af8a18e18a9e432a89b5fcbb4bb
Beatrice Gilfix <gilfixbea@gmail.com>
2025-01-14 14:26:35 -0800

    Documentation for bank project initial

--- Project/Bank/main/java/org/example/BankAccount.java
+++ Project/Bank/main/java/org/example/BankAccount.java
@@ -10,19 +10,36 @@ public class BankAccount {
     private Bank bank;
     private String ownerName;
 
+    /**
+     * Constructs bank account given inital balance and inital name
+     * @param balance Initial balance
+     * @param owner Initial name
+     */
+    //Longs should be used for something that needs extreme precision, like a bank-account balance!
     public BankAccount(long balance, String owner) {
         this.ownerName = owner;
         this.balance = balance;
     }
 
+    /**
+     * Changes the account's name
+     * @param ownerName New name to change account name to
+     */
     public void changeAccount(String ownerName) {
         this.ownerName = ownerName;
     }
 
+    /**
+     * Changes the account's interest rate
+     * @param interestRate New account interest rate
+     */
     public void changeInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    /**
+     * Apply account's internal intrestRate field to current balance
+     */
     public void applyInterest() {
         balance = (long) (balance * (interestRate/100 + 1));
     }
@@ -37,6 +54,10 @@ public class BankAccount {
         return Integer.parseInt(idCompile.toString());
     }
 
+    /**
+     * Report bank account information
+     * @return Bank account information as string
+     */
     @Override
     public String toString() {
         return "BankAccount{" +
